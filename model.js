f743fa8127d3b0766c5732dc58f40f3ad0bec7e9
hukai <532809319@qq.com>
2017-02-15 14:13:56 +0800

    多个模块引入

---
+++ model.js
@@ -0,0 +1,10 @@
+/**
+ * Created by hukai on 15/02/2017.
+ */
+var app2 = angular.module("myApp2", []);
+app2.controller("lastctr", ['$scope',
+    function lastctr($scope) {
+        $scope.name='2';
+
+    }
+]);
